7f0707e3c2a46518b1854784faa3f6a23b7babbd
andreighervan <andrei.ghervan8@gmail.com>
2021-02-19 16:51:52 +0200

    update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,6 +8,6 @@ import * as functions from "firebase-functions";
 //   response.send("Hello from Firebase!");
 // });
 
-const universal = require(`${process.cwd()}\dist\server`).app();
+const universal = require(`${process.cwd()}\\dist\\server`).app();
 
 export const ssr = functions.https.onRequest(universal);
