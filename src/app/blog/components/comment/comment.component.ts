06ae211e70a0968f962fb9801478499b2aa0ac6e
andreighervan <andrei.ghervan8@gmail.com>
2021-02-19 16:09:58 +0200

    comment component

--- src/app/blog/components/comment/comment.component.ts
+++ src/app/blog/components/comment/comment.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BlogService } from '../../services/blog.service';
-import { Comment } from '../../models/blog';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
@@ -10,47 +7,9 @@ import { Comment } from '../../models/blog';
 })
 export class CommentComponent implements OnInit {
 
-  @ViewChild('dynamic', { read: ViewContainerRef }) viewContainerRef: ViewContainerRef;
-
-  public comment: Comment;
-  comments$: Observable<Comment[]>;
-
-  COMMENTS_REF: string = "comments";
-  COL_NODE: string;
-
-  viewReply: string;
-  editComment: string;
-
-  constructor(private blogService: BlogService) {
-    this.comment = new Comment();
-    this.viewReply = "";
+  constructor() {
   }
 
   ngOnInit() {
-    this.COL_NODE = `${this.comment.docPath}/${this.COMMENTS_REF}`;
-    this.comments$ = this.blogService.colWithIds$(this.COL_NODE, ref => ref.orderBy('date', 'desc'));
   }
-
-  onComment(comment: Comment) {
-    this.blogService.addComments(comment, this.COL_NODE)
-      .then(_ => {
-        this.comment = new Comment();
-      })
-  }
-
-  onEdit(comment: Comment) {
-    this.blogService.updateComment(comment.docPath, comment)
-      .then(_ => {
-        this.editComment = "";
-      })
-  }
-
-  onDelete(comment: Comment) {
-    this.blogService.deleteComment(comment.docPath);
-  }
-
-  onViewReply(comment: Comment) {
-    this.blogService.addDynamicComponent(this.viewContainerRef, comment);
-  }
-
 }
--- src/app/blog/services/blog.service.ts
+++ src/app/blog/services/blog.service.ts
@@ -1,19 +1,14 @@
-import { ComponentFactoryResolver, ComponentRef, Injectable, ViewContainerRef } from '@angular/core';
-import { AngularFirestore, DocumentChangeAction } from '@angular/fire/firestore';
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { from, Observable } from 'rxjs';
-import { first, map, shareReplay, take } from 'rxjs/operators';
+import { first, map, } from 'rxjs/operators';
 import { Blog } from '../models/blog';
-
-import firebase from 'firebase/app'
-import { CommentComponent } from '../components/comment/comment.component';
-import { Comment } from '../models/blog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
-  constructor(private db: AngularFirestore, private factoryResolver: ComponentFactoryResolver) {
-    this.factoryResolver = factoryResolver;
+  constructor(private db: AngularFirestore) {
   }
 
   createBlogPost(value, file) {
@@ -67,66 +62,6 @@ export class BlogService {
       };
 
     });
-  }
-
-  // comments
-
-  get timestamp() {
-    return firebase.firestore.FieldValue.serverTimestamp();
-  }
-
-  addComments(data: any, ref?: any): Promise<firebase.firestore.DocumentReference> {
-    const timestamp = this.timestamp;
-    const copyTrans = Object.assign({}, data);
-    const data$ = JSON.parse(JSON.stringify(copyTrans));
-
-    return this.db.collection(ref).add({
-      ...data$,
-      updatedAt: timestamp,
-      createdAt: timestamp,
-    });
-  }
-
-  deleteComment<T>(ref: any): Promise<void> {
-    return this.db.doc(ref).delete();
-  }
-
-  updateComment<T>(ref: any, data: any): Promise<void> {
-    return this.db.doc(ref).update({
-      ...data,
-      updatedAt: this.timestamp,
-    });
-  }
-
-  addDynamicComponent(viewContainerRef: ViewContainerRef, comment: Comment) {
-
-    if (!viewContainerRef)
-      return;
-    viewContainerRef.clear();
-
-    const factory = this.factoryResolver.resolveComponentFactory(CommentComponent);
-    const component: ComponentRef<CommentComponent> = factory.create(viewContainerRef.parentInjector);
-    const currComponent = component.instance;
-
-    currComponent.comment = comment;
-    viewContainerRef.insert(component.hostView);
-  }
-
-  colWithIds$<T>(ref?: any, queryFn?): Observable<any[]> {
-    return this.db.collection<T>(ref, queryFn)
-      .snapshotChanges()
-      .pipe(
-        map((actions: DocumentChangeAction<T>[]) => {
-          return actions.map((a: DocumentChangeAction<T>) => {
-            const data: Object = a.payload.doc.data() as T;
-            const docId = a.payload.doc.id;
-            const docPath = a.payload.doc.ref.path;
-            data['docPath'] = docPath;
-            return { docId, docPath, ...data };
-          });
-        }),
-        shareReplay(1)
-      );
   }
 
 
